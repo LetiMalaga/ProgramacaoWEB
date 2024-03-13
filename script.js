581f2d087b68d8a8b8a27d0786402d1cae8c46cd
LetiMalaga <64732282+LetiMalaga@users.noreply.github.com>
2024-03-12 21:10:34 -0300

    commit final da aula
    Commit final da aula -> Mais commits serão feitos com os exercícios, mas esse abrange tudo o que foi feito na aula

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // alert("Hello World!"); // Alerta automático do JS
-
-let nota = 10;
+let nota = parseInt(prompt("insira uma nota: "))
 const nomeInstituicao = "UCB" // se a const não for declarada logo de início, ela não funciona
+const notaParaPassar = 7;
+const notaMaxima = 10; // number, não int
 
-console.log("nota: ", nota); // Valor vai aparecer no console da página web, não na página em si. 
+let notaMaximaAtingida = false;
+
+console.log("Nota: ", nota); // Valor vai aparecer no console da página web, não na página em si. 
 // Serve para debugar o código
 
-if(nota > 7){
     let situacao = "aprovado";
-    var nomeAluno = "joao"; // Variável acessável fora do escopo; Global. Causa comportamentos estranhos
-    console.log("situacao: ", situacao);
+
+if(nota >= notaParaPassar){
+//    let situacao = " aprovado"; 
+//    var nomeAluno = "joao"; // Variável acessável fora do escopo; Global. Causa comportamentos estranhos
+situacao = "aprovado";
+} else {
+    situacao = "reprovado";
 }
 
-console.log("nome: ", nomeAluno);
+if(nota === notaMaxima){
+    notaMaximaAtingida = true
+}
+
+console.log("Situacao: ", situacao);
+console.log("Nota Maxima Atingida: ", notaMaximaAtingida)
+// console.log("nome: ", nomeAluno);
 
 // situação é definida dentro do if
 // console.log("situacao ", situacao);
